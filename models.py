a5df5d3a006f07e706c62efff833275cd83c06b6
GinaHorch <reggiaus@hotmail.com>
2025-01-02 16:19:49 +0800

    Remove app to separate model and add

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from app import app
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class JobApplication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
